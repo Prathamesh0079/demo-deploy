d540cb3e6b4528d1592c9b180e01d91c935d3852
Prathamesh0079 <prathameshkakade009@gmail.com>
2025-09-17 13:54:44 +0530

    Initial commit: Flask demo app

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+import os
+
+app = Flask(_name_)
+
+@app.route("/")
+def home():
+    return "Prathamesh Kakade"
+
+if _name_ == "_main_":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
